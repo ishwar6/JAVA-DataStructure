a9d8c66b4105c99a2d10fb51d82aa86e01e2d6dd
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2019-02-12 21:26:14 +0530

    Update tree.c

--- tree.c
+++ tree.c
@@ -20,3 +20,22 @@ node->right = NULL;
 return(node); 
 } 
 
+int main() 
+{ 
+
+struct node *root = newNode(1); 
+
+	
+
+root->left	 = newNode(2); 
+root->right	 = newNode(3); 
+
+
+
+root->left->left = newNode(4); 
+
+
+getchar(); 
+return 0; 
+}
+
