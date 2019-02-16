0eeec5ff2dbdc0a8408160ac9ebe15783a839af7
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2019-02-16 08:13:18 +0530

    Update sort.java

--- sort.java
+++ sort.java
@@ -17,28 +17,3 @@ public class SortExample
 } 
 
 
-
-import java.util.Arrays; 
-import java.util.Collections; 
-
-public class SortExample 
-{ 
-	public static void main(String[] args) 
-	{ 
-		String arr[] = {"practice.geeksforgeeks.org", 
-						"quiz.geeksforgeeks.org", 
-						"code.geeksforgeeks.org"
-					}; 
-
-	
-		Arrays.sort(arr); 
-		System.out.printf("Modified arr[] : \n%s\n\n", 
-						Arrays.toString(arr)); 
-
-		// Sorts arr[] in descending order 
-		Arrays.sort(arr, Collections.reverseOrder()); 
-
-		System.out.printf("Modified arr[] : \n%s\n\n", 
-						Arrays.toString(arr)); 
-	} 
-} 
