da670c1c006c3fafde35fec7264432bf3cfa8a4b
Ishwar Jangid <41536582+ishwar6@users.noreply.github.com>
2019-02-18 10:15:12 +0530

    Update java_heap.java

--- java_heap.java
+++ java_heap.java
@@ -15,3 +15,18 @@ public class Memory {
 	} // Line 8
 
 }
+class Person {
+    int pid;
+    String name;
+     
+    // constructor, setters/getters
+}
+ 
+public class Driver {
+    public static void main(String[] args) {
+        int id = 23;
+        String pName = "Jon";
+        Person p = null;
+        p = new Person(id, pName);
+    }
+}
